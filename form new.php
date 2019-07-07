d14ed199d9e7c869a6dfd25ef73148a14d4fd8a9
Inna Kovalenko <vredinka025@gmail.com>
2019-07-07 11:19:04 +0300

    intermediate commit

---
+++ form new.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Форма</title>
+</head>
+<body>
+<h1>Получено:</h1>
+<table>
+<?php foreach ($_REQUEST as $field => $value): ?>
+    <tr>
+        <th><?= $field ?></th>
+        <td><?=htmlentities($value) ?></td>
+    </tr>
+<?php endforeach ?>
+</table>
